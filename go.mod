c10e133e609627bcbb6fef41f00bb8e51e7908e7
ReisenTM <s3068272040@gmail.com>
2025-04-28 22:09:13 +0800

    db

---
+++ go.mod
@@ -0,0 +1,23 @@
+module Spider-demo
+
+go 1.24
+
+require (
+	github.com/PuerkitoBio/goquery v1.10.3
+	gorm.io/driver/postgres v1.5.11
+	gorm.io/gorm v1.26.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"net/http"
+	"strconv"
+)
+
+type Data struct {
+	Title string `json:"title" `
+	Date  string `json:"date"`
+}
+
+type HuelNews struct {
+	Id    uint   `json:"id" gorm:"primarykey"`
+	Title string `json:"title"`
+	Date  string `json:"date"`
+}
+
+// 数据库连接初始化
+var mydb *gorm.DB
+
+const (
+	USERNAME = "c.c"
+	PASSWORD = ""
+	HOST     = "localhost"
+	PORT     = 5432
+	DATABASE = "postgres"
+)
+
+func initDB() error {
+	var err error
+	dsn := "host=localhost user='c.c' password='' dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	fmt.Printf("dsn: %s\n", dsn)
+	mydb, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), //设置全局日志
+	})
+	if err != nil {
+		return fmt.Errorf("failed to connect to database: %w", err)
+	}
+	// 自动创建数据库
+	if err := mydb.AutoMigrate(&HuelNews{}); err != nil {
+		return fmt.Errorf("failed to migrate database: %w", err)
+	}
+	return nil
+}
+
+func main() {
+	err := initDB()
+	if err != nil {
+		panic(err)
+	}
+	ReqUrl := "https://www.huel.edu.cn/index/xydt2.htm"
+	for i := 1; i < 72; i++ {
+		fmt.Printf("开始爬取第%d页\n", i)
+		Spider(ReqUrl)
+		fmt.Println("======第", i, "页爬取完成======")
+		ReqUrl = "https://www.huel.edu.cn/index/xydt2/" + strconv.Itoa(i) + ".htm"
+	}
+}
+
+var id uint = 1
+
+func Spider(url string) {
+	//1.发送请求
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
+	req.Header.Add("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	//2.解析网页
+	DetailDoc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	//3.获取节点信息
+
+	//body > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li:nth-child(1)
+	//body  > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li:nth-child(1)
+	//body > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li:nth-child(3)
+	//body > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li:nth-child(1) > a
+	//body > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li:nth-child(1) > a > span
+	DetailDoc.Find("body > div.inner.listbg > div > div.inner_right > div.zn_list > ul > li").
+		Each(func(i int, s *goquery.Selection) {
+			title := s.Find("a").Text()
+			date := s.Find("a > span").Text()
+			data := HuelNews{
+				Title: title,
+				Date:  date,
+			}
+			fmt.Printf("正在读取第 %d 个元素", i)
+			fmt.Println(data)
+			// 保存到数据库
+			if err := mydb.FirstOrCreate(&data).Error; err != nil {
+				fmt.Printf("保存第 %d 个元素失败: %v\n", i, err)
+			} else {
+				fmt.Printf("保存第 %d 个元素成功\n", i)
+			}
+		})
+
+	//4.保存内容
+}
+
+//正则表达式
+//func divideInfo()
